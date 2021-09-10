c57e82c1787a56853576a084a0528622e2ef62ce
WanshunLu <90298081+WanshunLu@users.noreply.github.com>
2021-09-11 04:32:23 +0800

    Add files via upload

---
+++ src/constants/constants.js
@@ -0,0 +1,34 @@
+export const projects = [
+  {
+    title: 'Ace High Card Game',
+    description: "Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application - from start to finish. The App is called Memories and it is a simple social media app that allows users to post interesting events that happened in their lives.",
+      image: '/images/111.jpg',
+      tags: ['Mongo', 'Express', 'React', 'Node'],
+    visit: 'https://github.com/WanshunLu/hello-world/tree/main/Aces%20High%20Card%20Game',
+    id: 0,
+  },
+  {
+    title: 'Pokemo',
+    description:"While building it you're going to learn many advanced React & JavaScript topics, as well as how to use Stripe for card transactions. On top of that, at the end of the video, you will have this unique and complex webshop app that you will be able to add to your portfolio. And trust me, e-commerce applications are impressive.",
+    image: '/images/Pokemo.png',
+    tags: ['React', 'JavaScript'],
+    visit: 'https://github.com/WanshunLu/hello-world/blob/main/pokemon%20(2).py',
+    id: 1,
+  },
+  {
+    title: 'Tuition Calculator App',
+    description: "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
+      image: '/images/hero_admissions_calculator_=.jpg',
+      tags: ['React', 'WebRTC'],
+    visit: 'https://github.com/WanshunLu/hello-world/blob/main/Tuition%20Calculation%20simplified%20version.py',
+    id: 2,
+  },
+  
+];
+
+export const TimeLineData = [
+  { year: 2015, text: 'Start Studying abroad', },
+  { year: 2018, text: 'Gratuated from High School', },
+  { year: 2019, text: 'Get into the university, Start learning Programming', },
+  { year: 2021, text: 'Still in the university, keep learning programming', },
+];
